8b9af0a64cbccdbbed0d3a32d3e2f32b6a1e8e18
murad <m.jamburia.websmart@gmail.comgit config --global user.email m.jamburia.websmart@gmail.com>
2022-11-16 19:31:42 +0400

    basic structure

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  semi: false,
+  trailingComma: 'es5',
+  singleQuote: true,
+  printWidth: 80,
+  tabWidth: 2,
+  jsxSingleQuote: true,
+  endOfLine: true,
+  singleQuote: true,
+}
